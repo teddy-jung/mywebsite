c51c0cc67b28849ed5f3e6fa8422e51f2e9076c0
TeddyJung <teddyjung@TeddyJungui-MacBookPro.local>
2020-05-24 15:32:31 +0900

    [Add] modified urls.py, render index.html - get to know urlpatterns, include function - make directory templates/blog then vi index.html

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete = models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    created_at = models.DateTimeField()
 
     def __str__(self):
         return self.title
